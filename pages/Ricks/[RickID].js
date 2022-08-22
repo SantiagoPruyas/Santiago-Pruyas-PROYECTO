f2b466b3c05b22d040ac196f44ce65eb7eaa58f5
Santiago Pruyas <spruyas@hotmail.com>
2022-08-22 00:28:55 -0300

    Retocando ruta Ricks

--- pages/Ricks/[RickID].js
+++ pages/Ricks/[RickID].js
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Head from 'next/head';
 import styles from '../../styles/Character.module.css';
+import { style } from "@mui/system";
 
 const RicksDetails = () => {
     const router = useRouter()
@@ -30,15 +31,15 @@ const RicksDetails = () => {
         <Head>
             <title>{character ? character.name:"Detalle de Personaje"}</title>
         </Head>
-        <h1> {character.name} </h1>
         <img src={character.image} className={styles.img}></img>
-        <p> Estado: {character.status} </p>
-        <p> Especie: {character.species} </p>
-        <p> Genero: {character.gender} </p>
-        <p> Lugar de Origen: {character.origin.name} </p>
-
-        <p> Actualmente esta en: {character.location.name}</p>
-
+        <div className={styles.text}>
+            <h1 className={styles.name}> {character.name} </h1>
+            <p> Estado: {character.status} </p>
+            <p> Especie: {character.species} </p>
+            <p> Genero: {character.gender} </p>
+            <p> Lugar de Origen: {character.origin.name} </p>
+            <p> Actualmente esta en: {character.location.name}</p>
+        </div>
     </div>: <p>Loading...</p>
 }
 
